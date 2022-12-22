7eb7e9c06fc76464ac6fb97b061aea30e77ee7d0
EunJungKim <dmswjd7281@naver.com>
2022-12-22 23:35:27 +0900

    fix: 로그인 검증 오류 수정

--- src/components/register/Signin.tsx
+++ src/components/register/Signin.tsx
@@ -26,7 +26,6 @@ const Signin = () => {
   const onChangeUser = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     const regexp = getRegexVerification(name as 'email' | 'password', value)
-
     setUser({ ...user, [name]: value })
     setUserValidation({ ...userValidation, [name]: regexp })
   }
@@ -48,29 +47,32 @@ const Signin = () => {
     if (getAccessToken()) {
       navigate('/todo')
     }
-  })
+  }, [])
 
   return (
     <WholeWrap>
-      <p>로그인</p>
-      <Input
-        type='email'
-        name='email'
-        id='email'
-        value={user.email}
-        placeholder='이메일을 입력해주세요'
-        onChange={onChangeUser}
-      />
-      <input
-        type='password'
-        name='password'
-        id='password'
-        value={user.password}
-        onChange={onChangeUser}
-      />
-      <Button onClick={handlLoginClick} disabled={isUserValidation}>
-        로그인
-      </Button>
+      <Title>로그인</Title>
+      <InputWhole>
+        <Input
+          type='email'
+          name='email'
+          id='email'
+          value={user.email}
+          placeholder='이메일을 입력해주세요'
+          onChange={onChangeUser}
+        />
+        <Input
+          type='password'
+          name='password'
+          id='password'
+          value={user.password}
+          placeholder='비밀번호를 입력해주세요'
+          onChange={onChangeUser}
+        />
+        <Button onClick={handlLoginClick} disabled={isUserValidation}>
+          로그인
+        </Button>
+      </InputWhole>
     </WholeWrap>
   )
 }
@@ -79,13 +81,33 @@ export default Signin
 
 const WholeWrap = styled.div`
   width: 100%;
-  height: 100%;
+  height: 800px;
   display: flex;
+  flex-direction: column;
+  margin: 0 auto;
 `
 
-const Input = styled.input``
+const Title = styled.h2`
+  text-align: center;
+`
+
+const InputWhole = styled.div`
+  flex-direction: column;
+  width: 10em;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Input = styled.input`
+  width: 10em;
+  height: 3em;
+`
 
 const Button = styled.button`
-  &:disabled {
+  width: 10em;
+  height: 3em;
+  background-color: red;
+  :disabled {
+    background-color: grey;
   }
 `
