b0734da8d2e8e6e40fb0449a045b0dd0bf763939
InvincibleGaren <86401105+InvincibleGaren@users.noreply.github.com>
2022-12-22 23:17:12 +0900

    style: todoItem 컴포넌트 이름 변경
    style: todoItem 컴포넌트 이름 변경

---
+++ src/components/todo/TodoItem.tsx
@@ -0,0 +1,20 @@
+import { Todo } from '../../pages/Todo'
+
+interface TodoItemProps {
+  todo: Todo
+  onDeleteTodo: (todoId: number) => void
+  onUpdateTodo: (todoId: number, text: string) => void
+  onCompleteTodo: (todoId: number) => void
+}
+
+const TodoItem = ({ todo, onDeleteTodo, onUpdateTodo, onCompleteTodo }: TodoItemProps) => {
+  return (
+    <li>
+      <span>{todo.id}</span>&nbsp;
+      <span>{todo.todo}</span>&nbsp;
+      <span>{String(todo.isCompleted)}</span>&nbsp;
+    </li>
+  )
+}
+
+export default TodoItem
