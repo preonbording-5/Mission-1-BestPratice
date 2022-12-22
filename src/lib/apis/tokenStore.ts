ae4a5ab3933d59e75a2d02336dd663b70a363ee8
Seongtaek Hwang <hst1993@hanmail.net>
2022-12-22 17:23:38 +0900

    feat: axios 인스턴스, 인터셉터 추가

---
+++ src/lib/apis/tokenStore.ts
@@ -0,0 +1,14 @@
+const ACCESS_TOKEN_KEY = 'token'
+
+export const getAccessToken = () => {
+  try {
+    return JSON.parse(localStorage.getItem(ACCESS_TOKEN_KEY) || '')
+  } catch (e) {
+    return ''
+  }
+}
+
+export const setAccessToken = (token: string) =>
+  localStorage.setItem(ACCESS_TOKEN_KEY, JSON.stringify(token))
+
+export const clearAccessToken = () => localStorage.removeItem(ACCESS_TOKEN_KEY)
