4d0e1fea3755e142f1a6d7c0abf427113a4cd764
vanillovin <vanillovin@gmail.com>
2022-12-22 16:56:19 +0900

    feat: 임시 데이터로 투두 상태 관리

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { useState } from 'react';
 
-const Todo = () => {
-  return <div>todo</div>
+export interface Todo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
 }
 
-export default Todo
+const DUMMY_TODOS: Todo[] = [
+  {
+    id: 1,
+    todo: '투두 1',
+    isCompleted: false,
+    userId: 1,
+  },
+  {
+    id: 2,
+    todo: '투두 2',
+    isCompleted: true,
+    userId: 1,
+  },
+  {
+    id: 3,
+    todo: '투두 3',
+    isCompleted: false,
+    userId: 1,
+  },
+];
+
+const Todo = () => {
+  const [todoData, setTodoData] = useState({
+    loading: false,
+    todos: DUMMY_TODOS,
+    error: null,
+  });
+  const { loading, todos, error } = todoData;
+
+  if (error) return <div>error</div>;
+
+  return !loading ? (
+    <ul>
+      {todos.map(({ id, todo, isCompleted }) => (
+        <li key={id} style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>
+          {todo}
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <div>loading...</div>
+  );
+};
+
+export default Todo;
