ec6a03a4582425802e38c2e7884b9cf7e0b1d93e
vanillovin <vanillovin@gmail.com>
2022-12-22 18:32:24 +0900

    feat: 투두 추가 로직 구현

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import { Todo } from '../../pages/Todo';
 
 interface TodoFormProps {
-  onAddTodo: (todo: Todo) => void;
+  onAddTodo: (newTodo: Todo) => void;
 }
 
 const TodoForm = ({ onAddTodo }: TodoFormProps) => {
+  const [todo, setTodo] = useState('');
+
+  const handleAddTodo = (e: React.FormEvent<HTMLFormElement | HTMLButtonElement>) => {
+    e.preventDefault();
+    const newTodo = {
+      id: Date.now(),
+      todo,
+      isCompleted: false,
+      userId: 1,
+    };
+    onAddTodo(newTodo);
+    setTodo('');
+  };
+
+  const handleChangeTodo = (e: React.ChangeEvent<HTMLInputElement>) => setTodo(e.target.value);
+
   return (
-    <form>
-      <input type='text' />
+    <form onSubmit={handleAddTodo}>
+      <input type='text' value={todo} onChange={handleChangeTodo} />
+      <button type='submit' onClick={handleAddTodo} disabled={todo.length < 2}>
+        추가
+      </button>
     </form>
   );
 };
