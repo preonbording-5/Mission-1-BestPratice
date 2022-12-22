429f0c056ecf131e37f6987bec9ee47ebb0113df
EunJung Kim <37800677+eunjung0613@users.noreply.github.com>
2022-12-22 22:11:05 +0900

    Feat/signin (#7)
    * feat: 로그인 페이지 로직 구현, utils 폴더 내 regex작업
    
    * fix: main 브랜치와의 충돌 해결중
    
    * fix: styled-components 파일 오류로 package 재다운로드

--- src/components/register/Signin.tsx
+++ src/components/register/Signin.tsx
@@ -1,5 +1,91 @@
+import React, { ChangeEvent, useEffect, useMemo, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { getAccessToken, setAccessToken } from '../../lib/utils/AcessTokenStore'
+import { User, UserValidation } from '../../lib/types/User.interface'
+import styled from 'styled-components'
+import getRegexVerification from '../../lib/utils/Getregex'
+
 const Signin = () => {
-  return null
+  const navigate = useNavigate()
+  const [user, setUser] = useState<User>({
+    email: '',
+    password: '',
+  })
+
+  const [userValidation, setUserValidation] = useState<UserValidation>({
+    email: false,
+    password: false,
+  })
+
+  const isUserValidation = useMemo(
+    () => !(userValidation.email && userValidation.password),
+    [userValidation.email, userValidation.password],
+  )
+
+  const onChangeUser = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    const regexp = getRegexVerification(name as 'email' | 'password', value)
+
+    setUser({ ...user, [name]: value })
+    setUserValidation({ ...userValidation, [name]: regexp })
+  }
+
+  const handlLoginClick = async () => {
+    try {
+      const response = 'api 호출 구간'
+      // await Auth.SignIn(user)
+      alert('로그인 성공')
+      setAccessToken(response)
+      // response.data.access_token
+      navigate('/todo')
+    } catch (e: any) {
+      alert(e.response?.data?.message || '로그인 실패')
+    }
+  }
+
+  useEffect(() => {
+    if (getAccessToken()) {
+      navigate('/todo')
+    }
+  })
+
+  return (
+    <WholeWrap>
+      <p>로그인</p>
+      <Input
+        type='email'
+        name='email'
+        id='email'
+        value={user.email}
+        placeholder='이메일을 입력해주세요'
+        onChange={onChangeUser}
+      />
+      <input
+        type='password'
+        name='password'
+        id='password'
+        value={user.password}
+        onChange={onChangeUser}
+      />
+      <Button onClick={handlLoginClick} disabled={isUserValidation}>
+        로그인
+      </Button>
+    </WholeWrap>
+  )
 }
 
 export default Signin
+
+const WholeWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
+
+const Input = styled.input``
+
+const Button = styled.button`
+  &:disabled {
+  }
+`
