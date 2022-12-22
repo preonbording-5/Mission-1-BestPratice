569ab0b3b41b565164e732f6cc6c271d8b98a365
azerose <83802141+azerose@users.noreply.github.com>
2022-12-22 22:38:47 +0900

    Update AcessTokenStore.tsx

--- src/lib/utils/AcessTokenStore.tsx
+++ src/lib/utils/AcessTokenStore.tsx
@@ -12,8 +12,5 @@ export const getAccessToken = () => {
 export const setAccessToken = (accessToken: string) =>
   localStorage.setItem(ACCESS_TOKEN, JSON.stringify(accessToken))
 
-<<<<<<< HEAD
+
 export const clearAccessToken = () => localStorage.removeItem(ACCESS_TOKEN)
-=======
-export const clearAccessToken = () => localStorage.removeItem(ACCESS_TOKEN)
->>>>>>> main
