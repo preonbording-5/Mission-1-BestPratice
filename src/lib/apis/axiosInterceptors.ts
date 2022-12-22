7a4714f0e92da3abbfe07492e40eb6fe80c03860
ShinjungOh <tlswjd9260@gmail.com>
2022-12-22 19:10:22 +0900

    feat: auth API 구현

--- src/lib/apis/axiosInterceptors.ts
+++ src/lib/apis/axiosInterceptors.ts
@@ -1,13 +1,11 @@
-import { AxiosInstance } from 'axios'
-import { getAccessToken } from './tokenStore'
+import { AxiosInstance } from 'axios';
+import { getAccessToken } from './tokenStore';
 
-export const setInterceptors = (axiosInstance: AxiosInstance) =>
-  axiosInstance.interceptors.request.use(
-    function (config) {
+export const setInterceptors = (axiosInstance: AxiosInstance) => {
+  axiosInstance.interceptors.request.use((config) => {
       config.headers!.Authorization = `Bearer ${getAccessToken()}`
       return config
     },
-    function (error) {
-      return Promise.reject(error)
-    },
-  )
+    (error) => Promise.reject(error));
+  return axiosInstance;
+};
