122d84da4ef57e8c1683bce5ffdc8a5b453ef837
innie <innie0526@gmail.com>
2022-12-23 16:11:28 +0900

    feat : register 페이지 로직 구현

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,43 @@
 import { useNavigate } from 'react-router-dom'
-import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
+import Signup from '../components/register/Signup'
+import Signin from '../components/register/Signin'
+import { getAccessToken } from '../lib/apis/tokenStore'
 
+const validation = {
+  id: (value: string) => value.includes('@'),
+  password: (value: string) => value.length >= 8,
+}
+
+type RegisterMode = 'signUp' | 'singIn'
 
 const Register = () => {
-  return <div>Register</div>
+  const navigate = useNavigate()
+  const [mode, setMode] = useState<RegisterMode>('signUp')
+  const [id, setId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const isIdError = !!id && !validation['id'](id)
+  const isPasswordError = !!password && !validation['password'](password)
+
+  const isInvalid = !!id || !!password || isIdError || isPasswordError
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+    if (isInvalid) {
+      return
+    }
+
+    mode === 'signUp' ? <Signup /> : <Signin />
+  }
+
+  useEffect(() => {
+    if (getAccessToken()) {
+      navigate('/todo')
+    }
+  }, [navigate])
+
+  return <></>
 }
 
 export default Register
