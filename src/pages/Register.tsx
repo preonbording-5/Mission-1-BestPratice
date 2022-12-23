5634fe35932ccaced3c194c688244153ec54abf9
innie0526 <112600139+innie0526@users.noreply.github.com>
2022-12-23 18:48:45 +0900

    feat : register 페이지 로직 구현 (#14)
    * feat : register 페이지 로직 구현
    
    * refactor : Register 코드 정리
    
    * fix : Register 코드수정
    
    Co-authored-by: innie <innie0526@gmail.com>

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,30 @@
 import { useNavigate } from 'react-router-dom'
-import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
+import Signup from '../components/register/Signup'
+import Signin from '../components/register/Signin'
+import { getAccessToken } from '../lib/apis/tokenStore'
 
+
+type RegisterMode = 'signUp' | 'singIn'
 
 const Register = () => {
-  return <div>Register</div>
+  const navigate = useNavigate()
+  const [mode, setMode] = useState<RegisterMode>('signUp')
+
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    if (getAccessToken()) {
+      navigate('/todo')
+    }
+  }, [navigate])
+
+  return <div>
+    {mode === 'signUp' ? <Signup/> : <Signin/>}
+  </div>
 }
 
 export default Register
